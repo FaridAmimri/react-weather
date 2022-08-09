f5444ca24d72b44ce6d025edef112f201f935a94
Farid Amimri <farid.amimri@gmail.com>
2022-08-09 23:10:41 +0200

    Ajout states location + isLoading + spinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 /** @format */
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { ClapSpinner } from 'react-spinners-kit'
 
 function App() {
   const [data, setData] = useState('')
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('Paris')
+  const [isLoading, setIsLoading] = useState(true)
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=22519734f5421c8f55450aaa03737461`
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data)
+        setLocation('')
+        setIsLoading(false)
+      })
+      .catch((error) => console.log(error))
+  }, [])
 
   const searchLocation = (event) => {
     const city = event.target.value
@@ -15,9 +28,10 @@ function App() {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
         setData(response.data)
-        console.log(response.data)
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + city + "')"
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?" + city + "')"
         setLocation('')
+        setIsLoading(false)
       })
     }
   }
@@ -33,41 +47,54 @@ function App() {
           type='text'
         />
       </div>
-      <div className='container'>
-        <div className='top'>
-          <div className='location'>
-            <p>{data.name}</p>
-          </div>
-          <div className='temp'>
-            {/* data.main ? => checking first if data.main is returned */}
-            {data.main ? <h1>{data.main.temp.toFixed()}°c</h1> : null}
-          </div>
-          <div className='description'>
-            {/* data.weather ? => checking first if data.weather is returned */}
-            {data.weather ? <p>{data.weather[0].description}</p> : null}
-          </div>
+      {isLoading && (
+        <div className='spinner'>
+          <ClapSpinner size={30} frontColor='#fff' />
         </div>
-
-        {data.name !== undefined && (
-          <div className='bottom'>
-            <div className='feels'>
-              {/* data.main ? => checking first if data.main is returned */}
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°c</p> : null}
-              <p>Feels like</p>
+      )}
+      {!isLoading && (
+        <div className='container'>
+          <div className='top'>
+            <div className='location'>
+              <p>{data.name}</p>
             </div>
-            <div className='humidity'>
+            <div className='temp'>
               {/* data.main ? => checking first if data.main is returned */}
-              {data.main ? <p className='bold'>{data.main.humidity.toFixed()}°c</p> : null}
-              <p>Humidity</p>
+              {data.main ? <h1>{data.main.temp.toFixed()}°c</h1> : null}
             </div>
-            <div className='wind'>
-              {/* data.wind ? => checking first if data.wind is returned */}
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()}km/h</p> : null}
-              <p>Wind Speed</p>
+            <div className='description'>
+              {/* data.weather ? => checking first if data.weather is returned */}
+              {data.weather ? <p>{data.weather[0].description}</p> : null}
             </div>
           </div>
-        )}
-      </div>
+
+          {data.name !== undefined && (
+            <div className='bottom'>
+              <div className='feels'>
+                {/* data.main ? => checking first if data.main is returned */}
+                {data.main ? (
+                  <p className='bold'>{data.main.feels_like.toFixed()}°c</p>
+                ) : null}
+                <p>Feels like</p>
+              </div>
+              <div className='humidity'>
+                {/* data.main ? => checking first if data.main is returned */}
+                {data.main ? (
+                  <p className='bold'>{data.main.humidity.toFixed()}°c</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className='wind'>
+                {/* data.wind ? => checking first if data.wind is returned */}
+                {data.wind ? (
+                  <p className='bold'>{data.wind.speed.toFixed()}km/h</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+      )}
     </div>
   )
 }
